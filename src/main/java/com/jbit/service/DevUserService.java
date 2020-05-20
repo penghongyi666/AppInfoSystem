4a3eaa35ffcb28e22a8622d0336e795ab5af0e9f
Mr peng <penghongyi666@qq.com>
2020-05-20 12:42:11 +0800

    first commit

---
+++ src/main/java/com/jbit/service/DevUserService.java
@@ -0,0 +1,29 @@
+package com.jbit.service;
+
+
+import com.jbit.mapper.DevUserMapper;
+import com.jbit.pojo.DevUser;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.handler.DefaultWebFilterChain;
+
+import javax.annotation.Resource;
+
+@Service
+@Transactional(propagation = Propagation.SUPPORTS)
+public class DevUserService {
+
+    @Resource
+    private  DevUserMapper devUserMapper;
+
+
+    //用户登录
+    public DevUser queryLogin(String devcode,String devpassword){
+        DevUser devUser =new DevUser();
+        devUser.setDevcode(devcode);
+        devUser.setDevpassword(devpassword);
+
+        return devUserMapper.selectOne(devUser);
+    }
+}
